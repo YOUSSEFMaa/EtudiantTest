c1ac135e8fcff0e83f9122aadfc244d6e342eccc
YOUSSEF MAAYAR <YOUSSEF MAAYAR@DESKTOP-9JM7J5G>
2018-11-25 15:32:17 +0000

    First Commit

---
+++ demo-github/src/main/java/org/devup/DemoGithubApplication.java
@@ -0,0 +1,12 @@
+package org.devup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGithubApplication.class, args);
+	}
+}
